8765553db4c894f2601e99fc275f680345e4c28d
Tawanda Munongo <ejmunongo@gmail.com>
2023-09-10 10:09:10 +0200

    latest addition is my qtile config

---
+++ qtile/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# qtile dir
+qdir="$HOME/.config/qtile"
+
+# Lauch polybar
+#polybar &
+
+# polkit
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Launch feh
+feh --bg-fill ~/Pictures/Wallies/6.jpg
+
